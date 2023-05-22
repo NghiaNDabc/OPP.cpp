5c1a328843fa19be6d64a49b9a7430f99d0a757e
NghiaNDabc <203nghiapham@gmail.com>
2023-05-23 00:45:26 +0700

    sua BN

--- BichNgoc/NguyenThiBichNgoc_2021607358_5.4 (1).cpp
+++ BichNgoc/NguyenThiBichNgoc_2021607358_5.4 (1).cpp
@@ -5,29 +5,32 @@ class MATRIX{
 	int n;
 	int m;
 	public: 
-		friend istream& operator >> (istream&in, MATRIX x);
+		friend istream& operator >> (istream&in, MATRIX &x);
 		friend ostream& operator << (ostream&out, MATRIX x);
 		MATRIX operator ! ();
 		MATRIX operator + (MATRIX y);
 		MATRIX operator - (MATRIX y);
 };
-istream& operator >> (istream&in, MATRIX x){
+istream& operator >> (istream&in, MATRIX &x){
 	cout<<"Nhap so hang: "; in>>x.n; 
 	cout<<"Nhap so cot: "; in>>x.m;
-	for(int i=0; i<x.n; i++){
 		x.a=new double *[x.n];
-		for(int j=0; j<x.m; j++){
+		for(int i=0; i<x.m; i++){
 			x.a[i]= new double [x.m];
-			in>>x.a[i][j];
 		}
-		cout<<endl;
-	}
+		for(int i=0; i<x.n; i++)
+		for(int j=0; j<x.m; j++)
+		{
+			cout << "a" << i << " " << j << " = ";
+			in >> x.a[i][j];	
+		}
+	
 	return in;
 }
 ostream& operator << (ostream&out, MATRIX x){
 	for(int i=0; i<x.n; i++){
 		for(int j=0; j<x.m; j++){
-			out<<x.a[i][j];
+			out<<x.a[i][j] << " ";
 		}
 		out<<endl;
 	}
@@ -74,10 +77,11 @@ int main(){
 	MATRIX a,b;
 	cout<<"Nhap ma tran: "<<endl;
 	cout<<"Ma tran 1: "<<endl; cin>>a; 
-	cout<<"Ma tran 2: "<<endl; cin>>b;
-	cout<<"2 ma tran vua tao: "<<a<<"\t"<<b<<endl; 
-	cout<<"Tong 2 ma tran: "<<a+b<<endl;
-	cout<<"Hieu 2 ma tran: "<<a-b<<endl;
+	//cout<<"Ma tran 2: "<<endl; cin>>b;
+	cout<<"ma tran vua tao: "<< endl <<a;
+//	cout<<"\t"<<b<<endl; 
+//	cout<<"Tong 2 ma tran: "<<a+b<<endl;
+//	cout<<"Hieu 2 ma tran: "<<a-b<<endl;
 }
 
 		
